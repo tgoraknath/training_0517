051f54c2ce80d00a1bd9c7fbb8ea0682bff7a318
Gorak <gtulla@walmartlabs.com>
2017-05-20 07:46:45 -0700

    added java oops module

---
+++ oops_nomvn/src/com/myorg/lang/FindAccessSpecifiersAndModifiers.java
@@ -0,0 +1,28 @@
+package com.myorg.lang;
+
+import java.util.Map;
+
+
+/**
+ * Below class has issues, identify all of them and add comment to each of it.
+ * 1. what is the issue and how to fix it.
+ * @author gtulla
+ *
+ */
+
+public class FindAccessSpecifiersAndModifiers {
+	public static Map<String, String> myMap;
+	private FindAccessSpecifiersAndModifiers() {
+		
+	}
+	public void add(String key, String value) {
+		myMap.put(key, value);
+	}
+	public String get(String key) {
+		return myMap.get(key);
+	}
+	public static String find(String key) {
+		return FindAccessSpecifiersAndModifiers.find(key);
+	}
+
+}
