95d5547d5d6cb070d05cc09bf5a31396f3b7c864
sreenavya9 <sreenavya9999@gmail.com>
2017-05-21 12:47:16 -0500

    added an  interface and modified App.java

--- oops/src/main/java/trainer_oops/oops/App.java
+++ oops/src/main/java/trainer_oops/oops/App.java
@@ -6,8 +6,11 @@ package trainer_oops.oops;
  */
 public class App 
 {
+	static final int F1=0;
+	int F2=1;
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	    	System.out.println( "the value of F2 is:" +Fibonacci.F2);
     }
 }
---
+++ oops/src/main/java/trainer_oops/oops/Fibonacci.java
@@ -0,0 +1,11 @@
+package trainer_oops.oops;
+
+public interface Fibonacci {
+	int F1 = 0; 
+	int F2 = 1;
+	int fibo(int a);
+	
+/* fibo 0 1 1 2 3 5 8 13....  */
+}
+
+/* access modifiers: public, private, protected, default */
